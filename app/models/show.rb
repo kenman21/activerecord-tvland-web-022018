a67b1902397308a5c303df36db84a578786b072b
github username <github email address>
2018-03-05 17:27:46 -0500

    Build all tables and relationships

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def self.build_network(arg)
+    Network.new(arg)
+  end
+
 end
